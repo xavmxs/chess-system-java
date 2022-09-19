75f1ae365910f67379d99a88120353d289db6f79
Vanessa Xavier <xavmxspro@gmail.com>
2022-09-19 09:52:06 -0300

    starting to implement Board and Piece

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,15 +1,15 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
+
 
 public class Application {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position (3,5);
-		System.out.println(pos);
-		System.out.println("");
-		System.out.println("tentando corrigir o erro");
+		Board board = new Board (8,8);
+		
+		
 		
 		
 		
